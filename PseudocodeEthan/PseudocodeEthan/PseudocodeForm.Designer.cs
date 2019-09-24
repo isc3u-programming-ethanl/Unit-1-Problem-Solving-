9b5fda7686dd7f16ab4893c4ce875ce2bd01dd12
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-09-24 13:06:54 -0400

    Updated Pseudocode

--- PseudocodeEthan/PseudocodeEthan/PseudocodeForm.Designer.cs
+++ PseudocodeEthan/PseudocodeEthan/PseudocodeForm.Designer.cs
@@ -28,21 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblInches = new System.Windows.Forms.Label();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblInches
+            // 
+            this.lblInches.AutoSize = true;
+            this.lblInches.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInches.Location = new System.Drawing.Point(52, 82);
+            this.lblInches.Name = "lblInches";
+            this.lblInches.Size = new System.Drawing.Size(461, 25);
+            this.lblInches.TabIndex = 0;
+            this.lblInches.Text = "Enter the diameter of the pizza (in inches):";
+            this.lblInches.Click += new System.EventHandler(this.LblInches_Click);
+            // 
+            // lblTax
+            // 
+            this.lblTax.AutoSize = true;
+            this.lblTax.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTax.Location = new System.Drawing.Point(52, 226);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(292, 25);
+            this.lblTax.TabIndex = 1;
+            this.lblTax.Text = "The cost (including tax) is:";
+            this.lblTax.Click += new System.EventHandler(this.LblTax_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(57, 142);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(206, 56);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // txtDiameter
+            // 
+            this.txtDiameter.Location = new System.Drawing.Point(535, 87);
+            this.txtDiameter.Name = "txtDiameter";
+            this.txtDiameter.Size = new System.Drawing.Size(100, 20);
+            this.txtDiameter.TabIndex = 3;
+            this.txtDiameter.TextChanged += new System.EventHandler(this.TxtDiameter_TextChanged);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(387, 226);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(51, 25);
+            this.lblAnswer.TabIndex = 4;
+            this.lblAnswer.Text = "???";
+            this.lblAnswer.Click += new System.EventHandler(this.LblAnswer_Click);
             // 
             // frmPseudocode
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtDiameter);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.lblInches);
             this.Name = "frmPseudocode";
             this.Text = "Pseudocode by Ethan";
             this.Load += new System.EventHandler(this.FrmPseudocode_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInches;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.Label lblAnswer;
     }
 }
 
--- PseudocodeEthan/PseudocodeEthan/PseudocodeForm.cs
+++ PseudocodeEthan/PseudocodeEthan/PseudocodeForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Ethan L
+ * Created on: 09/24/19
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #12 - Pizza Cost
+ * This program calculates the cost of a pizza with the diameter and then tax
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,6 +25,44 @@ namespace PseudocodeEthan
         }
 
         private void FrmPseudocode_Load(object sender, EventArgs e)
+        {
+            this.lblAnswer.Hide();
+        }
+
+        private void LblInches_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtDiameter_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            double diameter, costBeforeTax, costAfterTax;
+
+            // convert the diameter to double
+            diameter = double.Parse(txtDiameter.Text);
+
+            // calculate cost before and after tax
+            costBeforeTax = 0.75 + 0.99 + 0.50 * diameter;
+            costAfterTax = costBeforeTax * 1.13;
+
+            // display the cost as a label and rounded to two decimals
+            lblAnswer.Text = String.Format("${0:0.00}", costAfterTax);
+
+            // hide the answer text
+            this.lblAnswer.Show();
+        }
+
+        private void LblTax_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblAnswer_Click(object sender, EventArgs e)
         {
 
         }
